a4c19c517316ee6a130287a4b7d2962d542940d8
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2019-05-17 15:40:39 +0200

    Some cleanup

--- scripts/SimplePeer.gd
+++ scripts/SimplePeer.gd
@@ -5,13 +5,18 @@ var automate = true
 var channels = []
 
 func _ready():
-	peer = WebRTCPeerConnection.new()
+	initialize_peer()
+	peer.connect("ice_candidate_created", self, "new_ice_candidate")
+	peer.connect("session_description_created", self, "offer_created")
+	peer.connect("data_channel_received", self, "data_channel_received")
+
+func initialize_peer():
 	peer.initialize({
 		"iceServers": [
-			{
+			{ # STUN
 				"urls": ["stun:stun.l.google.com:19302", "stun:stun.2.google.com:19302"]
 			},
-#			{
+#			{ # Or TURN
 #				"username": "fales",
 #				"credential": "mypassword",
 #				"urls": ["turn:127.0.0.1:3478"],
@@ -21,24 +26,18 @@ func _ready():
 	channels.append(peer.create_data_channel("negotiated1", {
 		"ordered": false,
 		"maxPacketLifeTime": 200,
-		#"maxRetransmits": 5,
 		"negotiated": true,
 		"protocol": "myCustomProtocol",
 		"id": 10
 	}))
 	channels.append(peer.create_data_channel("negotiated2", {
 		"ordered": true,
-		#"maxPacketLifeTime": 100,
 		"maxRetransmits": 1,
 		"negotiated": true,
 		"protocol": "myCustomProtocol2",
 		"id": 2
 	}))
-	_log(str(channels[0].get_ready_state()))
 	LocalServer.register(get_path())
-	peer.connect("ice_candidate_created", self, "new_ice_candidate")
-	peer.connect("session_description_created", self, "offer_created")
-	peer.connect("data_channel_received", self, "data_channel_received")
 
 func data_channel_received(p_channel : WebRTCDataChannel):
 	_log("Received channel: %s - label: %s" % [p_channel, p_channel.get_label()])
@@ -113,13 +112,12 @@ func _on_IceCandidate_pressed():
 
 func _on_Button_pressed():
 	print("pressed")
-	channels.append(peer.create_data_channel("auto", {}))
+	channels.append(peer.create_data_channel("auto"))
 	channels.append(peer.create_data_channel("inband", {
 		"ordered": false,
 		"maxPacketLifeTime": 100,
 		"protocol": "myProtocol",
 	}))
-#	channels.append(peer.create_data_channel("test2", {}))
 	peer.create_offer()
 
 func _on_Automate_toggled(button_pressed):
@@ -127,4 +125,5 @@ func _on_Automate_toggled(button_pressed):
 
 func _on_Close_pressed():
 	peer.close()
-	peer.initialize()
+	channels.clear()
+	initialize_peer()
